951e88160fe0660fb55654ad413d8e976fa5fc67
elektroniz <elektroniz@protonmail.com>
2022-04-10 13:56:27 -0400

    RTCP-2: added etherparse for parsing incoming slice on buffer

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,9 @@
-// @elektroniz -| implementing TCP in RUST
-// Versioning: 0.1
-// Starting development date: 10/04/2022
-
-// Before reading the code, just consider what
-// is TCP.
-// TCP allows hosts to send data in such a way
-// to garantee no losing data among partners.
-// Our main purpose is implementing something
-// that works in that way.
-
 use std::io;
 
 fn main() -> io::Result<()> {
-    // nic = network interface card.
-    // tun0 because is a virtual interface card. tun stay for "tunnel".
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buffer = [0u8; 1504];
-     
+
     loop {
         let _nbytes = nic.recv(&mut  buffer[..])?;
         let _eth_flags  = u16::from_be_bytes([buffer[0], buffer[1]]);
@@ -25,7 +12,22 @@ fn main() -> io::Result<()> {
         let ipv4_proto = 0x0800;
         if eth_proto != ipv4_proto { continue; }
 
-        println!("communicating throught ipv4_proto of {}", _nbytes);
-        
+        match etherparse::Ipv4HeaderSlice::from_slice(&buffer[4.._nbytes]) {
+            Ok(packet) =>  {
+ 
+                println!("{:?} -> {:?}: - Read {} bytes (proto: {:x})",
+                    packet.source_addr();
+                    packet.destination_addr();
+                    packet.payload_len(),
+                    packet.protocol()
+                );
+                let source_address      = packet.source_addr();
+                let destination_address = packet.destination_addr();
+
+            }
+            Err(e) => {
+                println!("Warning: weird packet {:?}", e)
+            }
+        }
     }
 }
